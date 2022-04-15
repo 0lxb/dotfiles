bc746a24009c96914553383078a896ab87b3c5e1
Xiaobing Luo <luoxiaobing0926@gmail.com>
2022-04-15 15:17:01 +0800

    add add_profile.sh

---
+++ scripts/add_profile.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cat < EOF >> ~/.profile
+alias rm='safe-rm -i --preserve-root'
+alias lf='du -d 1 -h'
+alias lh='ls -alhtF'
+alias rm -rf='rm -rf -i --preserve-root'
+alias vfzf='vim $(fzf)'
+
+# https://github.com/sharkdp/fd
+export FZF_DEFAULT_COMMAND="fd --exclude={.gidecodass-cache,node_modules} --type f"
+# https://github.com/junegunn/fzf
+export FZF_DEFAULT_OPTS="--preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -800'"
+EOF
